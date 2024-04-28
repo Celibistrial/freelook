26cb1e663c73fbd1c328cafa90f447c5929675ea
BoBkiNN <bobkin.yangirov@gmail.com>
2024-04-29 00:15:28 +0500

    Update to 1.20.5, reformat code, rename classes to match java naming conventions, remove unused code, optimize some code

--- src/main/java/freelook/freelook/FreelookScreen.java
+++ src/main/java/freelook/freelook/FreelookScreen.java
@@ -1,11 +1,8 @@
 package freelook.freelook;
 
-import com.google.gson.JsonObject;
-import freelook.freelook.client.FreelookmodClient;
-import net.minecraft.client.gui.DrawContext;
+import freelook.freelook.client.FreeLookModClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 
 import java.io.File;
@@ -13,84 +10,70 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class FreelookScreen extends Screen {
-    public FreelookScreen(Text title) {
-        super(title);
-    }
 
     public FreelookScreen() {
         super(Text.of("Freelook Config"));
     }
 
-    public void init(){
+    private Text getModeText(){
         String msg;
-        if(FreelookmodClient.isToggle){
+        if (FreeLookModClient.isToggle) {
             msg = "Freelook is set to toggle";
-        }
-        else {
+        } else {
             msg = "Freelook is set to hold";
         }
-        this.addDrawableChild(new ButtonWidget.Builder(Text.of(msg), (button) -> {
-            JsonObject value = new JsonObject();
-            if(FreelookmodClient.isToggle){
-                FreelookmodClient.isToggle = false;
+        return Text.of(msg);
+    }
+
+    private Text getPerspectiveText(int perspective){
+        var msg = switch (perspective) {
+            case 1 -> "Freelook is set to first person";
+            case 2 -> "Freelook is set to second person";
+            case 3 -> "Freelook is set to third person";
+            default -> "something went wrong click on me to fix it";
+        };
+        return Text.of(msg);
+    }
+
+    public void init() {
+        var btn = new ButtonWidget.Builder(getModeText(), button -> {
+            if (FreeLookModClient.isToggle) {
+                FreeLookModClient.isToggle = false;
                 File fee = new File("t.txt");
-
                 fee.delete();
-
-            }else {
-                FreelookmodClient.isToggle = true;
-
-                try {
-                    FileWriter fe = new FileWriter("t.txt");
-                    fe.write(".");
-                    fe.flush();
+            } else {
+                FreeLookModClient.isToggle = true;
+                try (var fw = new FileWriter("t.txt")) {
+                    fw.write(".");
+                    fw.flush();
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    FreeLookMod.LOGGER.error("Failed to write toggle true", e);
                 }
-
-
             }
-            Screen screen = new FreelookScreen();
-            assert this.client != null;
-            this.client.setScreen(screen);
+            button.setMessage(getModeText());
 
-        }).dimensions(this.width / 2 - 100, this.height/2-100, 200, 20).build());
-        var wrapper = new Object(){ int perspective = 3; };
+        }).dimensions(this.width / 2 - 100, this.height / 2 - 100, 200, 20).build();
+        this.addDrawableChild(btn);
+        var wrapper = new Object() {
+            int perspective = 3;
+        };
         VariableStorage variableStorage = new VariableStorage();
         try {
             wrapper.perspective = variableStorage.read();
         } catch (IOException e) {
-            e.printStackTrace();
+            FreeLookMod.LOGGER.error("Failed to read perspective", e);
         }
-        msg = switch (wrapper.perspective) {
-            case 1 -> "Freelook is set to first person";
-            case 2 -> "Freelook is set to second person";
-            case 3 -> "Freelook is set to third person";
 
-            default -> "something went wrong click on me to fix it";
-        };
-
-
-        this.addDrawableChild(new ButtonWidget.Builder(Text.of(msg), (button) -> {
-            if(!(wrapper.perspective >= 3)) {
+        this.addDrawableChild(new ButtonWidget.Builder(getPerspectiveText(wrapper.perspective), button -> {
+            if (!(wrapper.perspective >= 3)) {
                 wrapper.perspective += 1;
-            }else wrapper.perspective = 1;
+            } else wrapper.perspective = 1;
             try {
                 variableStorage.write(wrapper.perspective);
             } catch (IOException e) {
-                e.printStackTrace();
+                FreeLookMod.LOGGER.error("Failed to write perspective", e);
             }
-            Screen screen = new FreelookScreen();
-            assert this.client != null;
-            this.client.setScreen(screen);
-
-        }).dimensions(this.width / 2 - 100, this.height/2-50, 200, 20).build());
-
-    }
-
-    @Override
-    public void render(final DrawContext drawContext, final int mouseX, final int mouseY, final float delta) {
-        this.renderBackground(drawContext,mouseX,mouseY,delta);
-        super.render(drawContext, mouseX, mouseY, delta);
+            button.setMessage(getPerspectiveText(wrapper.perspective));
+        }).dimensions(this.width / 2 - 100, this.height / 2 - 50, 200, 20).build());
     }
 }
--- src/main/java/freelook/freelook/mixin/CameraMixin.java
+++ src/main/java/freelook/freelook/mixin/CameraMixin.java
@@ -1,42 +1,44 @@
 package freelook.freelook.mixin;
 
 
-
 import freelook.freelook.CameraOverriddenEntity;
-import freelook.freelook.client.FreelookmodClient;
+import freelook.freelook.client.FreeLookModClient;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.Camera;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.world.BlockView;
+
 @Mixin(Camera.class)
 public abstract class CameraMixin {
-    boolean firsttime = true;
+    @Unique
+    boolean firstTime = true;
+
     @Shadow
     protected abstract void setRotation(float yaw, float pitch);
 
     @Inject(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/Camera;setRotation(FF)V", ordinal = 0, shift = At.Shift.AFTER))
     public void lockRotation(BlockView focusedBlock, Entity cameraEntity, boolean isThirdPerson, boolean isFrontFacing, float f, CallbackInfo ci) {
-        if (FreelookmodClient.isFreeLooking && cameraEntity instanceof ClientPlayerEntity) {
+        if (FreeLookModClient.isFreeLooking && cameraEntity instanceof ClientPlayerEntity) {
             CameraOverriddenEntity cameraOverriddenEntity = (CameraOverriddenEntity) cameraEntity;
 
-            if(firsttime && MinecraftClient.getInstance().player != null){
+            if (firstTime && MinecraftClient.getInstance().player != null) {
                 cameraOverriddenEntity.setCameraPitch(MinecraftClient.getInstance().player.getPitch());
                 cameraOverriddenEntity.setCameraYaw(MinecraftClient.getInstance().player.getYaw());
-                firsttime = false;
+                firstTime = false;
             }
             this.setRotation(cameraOverriddenEntity.getCameraYaw(), cameraOverriddenEntity.getCameraPitch());
 
         }
-        if(!FreelookmodClient.isFreeLooking && cameraEntity instanceof ClientPlayerEntity){
-            firsttime = true;
+        if (!FreeLookModClient.isFreeLooking && cameraEntity instanceof ClientPlayerEntity) {
+            firstTime = true;
         }
     }
 
